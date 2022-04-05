4293d64884a2039447a616ed2ff9c75369aa6031
Andrew Arcbhold <pulsegrenade@gmail.com>
2022-04-04 17:29:07 -0700

    removing the format script as it messes up img src tags

--- src/components/hello-world/index.jsx
+++ src/components/hello-world/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import style from './hello-world.css';
+import homeLogo from '../../images/logos/ethos-logo.png';
 
 const HelloWorld = ({ title }) => (
-  <div className={style['hello-world']}>{title}</div>
+  <div>
+    <div className={style['hello-world']}>{title}</div>
+    <img src={homeLogo} alt="home logo" width="200" />
+    <img src="https://helios.party/static/images/bg-hero.jpg" alt="home logo" width="340" />
+    
+  </div>
 );
 
 HelloWorld.propTypes = {
